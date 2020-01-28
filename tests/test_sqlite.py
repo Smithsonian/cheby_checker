ebf6e6d26eac77decb7f6cb1703c009cbf9fa418
matthewjohnpayne <matthewjohnpayne@gmail.com>
2020-01-28 18:04:42 -0500

    tests

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -111,9 +111,50 @@ def test_tracklet_query():
     cur.execute('SELECT name from sqlite_master WHERE type = "table" AND name = "tracklets"')
     assert cur.fetchone() == None , 'table still exists'
 
+def test_tracklet_delete():
+    
+    # set up db & table
+    conn = sql.create_connection( sql.fetch_db_filepath() )
+    cur = conn.cursor()
+    sql.create_specific_table(conn)
+    
+    # create some data and then upload it ...
+    jd =123
+    hp = 456
+    
+    tracklet_name1 = 'kjhdfasdf'
+    tracklet_dict1 = {'asd':'fgh' , 'ghfgh':888}
+    sql.upsert_tracklet(conn, jd, hp, tracklet_name1, tracklet_dict1)
+    
+    tracklet_name2 = 'uituyiu'
+    tracklet_dict2 = {'asd':'gdhjdhjdhj' , 'ghfgh':985421}
+    sql.upsert_tracklet(conn, jd, hp, tracklet_name2, tracklet_dict2)
+    
+    # query the data & check that two dictionaries are returned
+    result = sql.query_tracklets_jdhp(conn, jd, hp)
+    assert isinstance(result, dict) and len(result) == 2
+    assert tracklet_name1 in result and tracklet_name2 in result
+    assert isinstance(result[tracklet_name1] , dict)
+    assert isinstance(result[tracklet_name2] , dict)
+    
+    # now delete a tracklet & check that only one dictionary is subsequently returned
+    sql.delete_tracklet(conn, tracklet_name1)
+    result = sql.query_tracklets_jdhp(conn, jd, hp)
+    assert isinstance(result, dict) and len(result) == 1
+    assert tracklet_name1 not in result and tracklet_name2 in result
+    assert isinstance(result[tracklet_name2] , dict)
+    
+    # Drop the table to facilitate future testing
+    cur.execute("DROP TABLE tracklets;")
+    cur.execute('SELECT name from sqlite_master WHERE type = "table" AND name = "tracklets"')
+    assert cur.fetchone() == None , 'table still exists'
+
+
 
 # Call the tests while developing ...
 test_db_creation()
 test_table_creation()
 test_tracklet_upsert()
 test_tracklet_query()
+test_tracklet_delete()
+
