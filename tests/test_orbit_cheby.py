c09024c0dbf4144406db8c664cedb3c02266fcda
matthewjohnpayne <matthewjohnpayne@gmail.com>
2020-03-31 16:47:21 -0400

    starting to build-up tests

--- tests/test_orbit_cheby.py
+++ tests/test_orbit_cheby.py
@@ -21,8 +21,14 @@ import pytest
 
 # Import neighboring packages
 # --------------------------------------------------------------
-from orbit_cheby import orbit_cheby
-from orbit_cheby import nbody_reader
+# Can't get the damn thing to work from symbolic link in ...
+# '/Users/matthewjohnpayne/opt/anaconda3/lib/python3.7/site-packages/orbit_cheby/__init__.py'
+# So just jacking this in for now ...
+sys.path.append( os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'orbit_cheby') )
+#os.chdir('/Users/matthewjohnpayne/Envs/orbit_cheby/orbit_cheby')
+
+import orbit_cheby
+import nbody_reader
 
 
 
@@ -30,7 +36,27 @@ from orbit_cheby import nbody_reader
 # Convenience data / functions to aid testing
 # --------------------------------------------------------------
 # Set up a filepath (file will be created during testing)
-test_filepath = os.path.join(os.path.dirname(os.getcwd() ), 'dev_data', '2022AA_demo.txt')
+test_filepath        = os.path.join(os.path.dirname(os.getcwd() ), 'dev_data', '2022AA_demo.txt')
+name, times, states  = nbody_reader.parse_nbody_txt( test_filepath )
+
+def create_single_MSC_from_array() :
+    return orbit_cheby.MSC_Loader(FROM_ARRAY = True ,
+                                 unpacked_provisional_designations = name,
+                                 times_TDB = times,
+                                 statearray = states).MSCs[0]
+
+def create_list_of_MSCs_from_arrays(n=5) :
+    arrays, names = [],[]
+    for i in range(1,n):
+        name, times, states  = nbody_reader.parse_nbody_txt( text_filepath )
+        names.append(name+"_"+str(i))
+        arrays.append(states)
+    states_3D = np.stack(arrays, axis=2)
+
+    return orbit_cheby.MSC_Loader(FROM_ARRAY = True ,
+                                     unpacked_provisional_designations = names,
+                                     times_TDB = times,
+                                     statearray = states_3D).MSCs
 
 
 # Actual tests ...
@@ -53,13 +79,14 @@ def test_text_file_creation(test_filepath):
 
 
 
-@pytest.mark.parametrize(('text_filepath'), [test_filepath])
-def test_generate_multi_sector_cheby_dict_from_nbody_text(text_filepath):
+def test_create_empty_MSC():
     
-    # Generate the multi_sector_cheby_dict_from_nbody_text
-    result = generate_multi_sector_cheby_dict_from_nbody_text(text_filepath)
+    # Initialize the multi_sector_cheby
+    result = orbit_cheby.MSC()
 
     # Check the result is as expected
-    assert isinstance( result , dict )
+    assert isinstance( MSC , orbit_cheby.MSC )
+    assert 'sector_coeffs' in MSC.__dict__
+    assert isinstance( MSC.sector_coeffs , list )
 
 
