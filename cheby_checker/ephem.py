d8ca7161102a825b219f5734d5b4fe55f814c960
matthewjohnpayne <matthewjohnpayne@gmail.com>
2020-08-04 09:34:50 -0400

    Sketching out stub functions for ephemeris and checking

--- cheby_checker/ephem.py
+++ cheby_checker/ephem.py
@@ -28,10 +28,16 @@
 # Import third-party packages
 # --------------------------------------------------------------
 import sys, os
+import numpy as np
 
 # Import neighboring packages
 # --------------------------------------------------------------
+try:
+    import orbit_cheby
+except ImportError:
+    from . import orbit_cheby
 
+from data_classes import detn_var_names, Detections
 
 # Ephemeris Object
 # --------------------------------------------------------------
@@ -61,17 +67,21 @@ class Ephem():
         self.times              = np.asarray(times)
         assert self.observatoryXYZ.shape == (self.times.shape[0],3)
 
-        # We'll want to fetch the minimal sub-set of data from the database : establish the required sectors
+
+        # Establish the minimal required set of sectors to be fetched from the database :
         # Turn dates into minimal set of sectors
         sector_numbers = np.asarray(sorted(set( orbit_cheby.Base.map_JD_to_sector_number( self.obsJDs , orbit_cheby.Base.standard_MJDmin) ) ) )
         # Add-in the adjacent sectors (just in case)
         sector_numbers = np.unique(np.concatenate([sector_numbers-1,sector_numbers,sector_numbers+1]))
         # Ensure that all requested sectors are supported
-        minAllowed,maxallowed = *orbit_cheby.Base.map_JD_to_sector_number( [orbit_cheby.Base.standard_MJDmin,orbit_cheby.Base.standard_MJDmax] , orbit_cheby.Base.standard_MJDmin)
+        minAllowed,maxallowed = orbit_cheby.Base.map_JD_to_sector_number( [orbit_cheby.Base.standard_MJDmin,orbit_cheby.Base.standard_MJDmax] , orbit_cheby.Base.standard_MJDmin)
         sector_numbers = sector_numbers[ (sector_numbers>=minAllowed) & (sector_numbers<=maxallowed) ]
         
-        # Use the *get_specific_object()* function in PreCalc to return the required list of MSCs
-        self.MSCs = [precalc.Precalc.get_specific_object(desig , sector_numbers = sector_numbers) for desig in self.designations]
+        # Use MSC_Loader to return the required list of MSCs
+        self.MSCs = orbit_cheby.MSC_Loader(
+                                           FROM_DB = True ,
+                                           primary_unpacked_provisional_designations = self.designations,
+                                           sectors = sector_numbers)
 
 
     # Convenience functions to generate various ephemeris quantities
@@ -79,18 +89,39 @@ class Ephem():
     def generate_sky_predictions( ):
         '''
            Get the on-sky RA,Dec & associated uncertainties
+           
            At some point should probably extend to Rates-of-Motion
+           
         '''
-
+        self.prediction_dict = {}
         for M in self.MSCs:
             
             # Get the RA & Dec
-            RADEC    = self.M.generate_RaDec( self.obsJDs  , observatoryXYZ=self.obsXYZ)#, APPROX = False , DELTASWITCH = False, delta=np.array([0,0,0]))
+            RADEC    = self.M.generate_RaDec( self.obsJDs  , observatoryXYZ=self.obsXYZ)
+            #, APPROX = False , DELTASWITCH = False, delta=np.array([0,0,0]))
     
             # Get the covariance matrix in RA & Dec
             covRADEC = self.M.covRaDec( self.obsJDs , self.obsXYZ )
 
-            # Look at posn_check _generate_predictions) to populate the rest of this function 
+            # Create pseudo-detections from the calculated positions ...
+            # Set-up an empty Detections instance of the correct length ...
+            Ds = Detections(len(self.times))
+            
+            # (ii) copy across the times & observatory-positions
+            Ds.D[:, detn_var_names['obstime']] = self.times
+            Ds.D[:, np.array([detn_var_names['pos1'], detn_var_names['pos2'],detn_var_names['pos3']]) ] = self.observatoryXYZ
+            
+            # (iii) populate the ra, dec & associated uncert
+            Ds.D[:,detn_var_names['ra']]     = RADEC[:,0]
+            Ds.D[:,detn_var_names['dec']]    = RADEC[:,1]
+            Ds.D[:,detn_var_names['rmsra']]  = covRADEC[:,11] ## <<-- These positions need to be updated !!!!!
+            Ds.D[:,detn_var_names['rmsdec']] = covRADEC[:,22]
+
+            # Store the Detections object in a dictionary
+            self.prediction_dict[ Ds ]
+
+        return self.prediction_dict
+
 
 
 
--- cheby_checker/mpchecker2.py
+++ cheby_checker/mpchecker2.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
-# mpchecker2/mpchecker2/mpchecker2.py
+# cheby_checker/cheby_checker/mpchecker2.py
 
 '''
 --------------------------------------------------------------
-Main MPChecker2 module.
+The main "checking" functionalities
 
-Feb 2020
+Jul 2020
 Matt Payne
 
-To contain all (high-level) functions required to predict
-minor planets within the field-of-view of a pointing
-
 As currently sketched-out 
-(i)     Pre-Calculations are handled in mpchecker2/mpchecker2/precalc.py
-(ii)    MPCHECKER class takes POINTINGs as input and then
-        calculates the SUSPECTS within the FoV
-(iii)   IDENTIFY class takes SUSPECTS and compares them to data
-        (either EXPOSURE or TRACKLETS) and returns IDENTIFICATIONS
+(i)     Pre-Calculations are handled in precalc.py
+(ii)    The Check class provides the methods to undertake
+        (a) pCheck == posn_check == Are the supplied
+            observations consistent with the predicted posns
+            for the named object
+        (b) mpchecker == what objects are within the FoV of
+            a given pointing
+        (c) checkid == what known object would have a posn
+            consistent with the supplied detections
+        (d) checkidX == was per checkid, but allowing the
+            mean anomaly to vary
+            
+*** AS OF 2020_07_31, MUCH OF THE CODE IN HERE SHOULD BE    ***
+*** VIEWED AS PSEUDO-CODE, JUST SKETCHING OUT APPROXIMATELY ***
+*** HOW IT COULD/SHOULD WORK. NO TESTING HAS BEEN DONE      ***
 
 --------------------------------------------------------------
 '''
@@ -26,232 +33,156 @@ As currently sketched-out
 # --------------------------------------------------------------
 import sys
 import numpy as np
+from astropy_healpix import healpy
 
 
 # Import neighboring packages
 # --------------------------------------------------------------
 import precalc
-#import orbit_cheby as eph
+from orbit_cheby import Base
+import data_classes
 
 
-# Data classes for MPChecker2
+
+# Main functional classes for ...
 # --------------------------------------------------------------
 
-class Pointing(namedtuple('Pointing', 'obstime ra dec radius pos1 pos2 pos3')):
+class Check(Base):
     '''
-        To contain the pointing (exposure) which is the input to MPChecker
-        
-        Will have to contain information like ...
-        date,
-        obscode/observatory-posn,
-        (RA,Dec)/Unit-Vector,
-        radius,
-        ...
-        
-        Adding methods to namedtuple (also don't use dict)
-        http://zetcode.com/python/namedtuple/
-        
-    '''
-    
-    # Magic to prevent use of dict by class ...
-    __slots__ = ()
-    
-    #@property
-    #def UnitVec(self):
-    #    return sqrt((self.x ** 2 + self.y ** 2))
-    
-    # Define a string
-    def __str__(self):
-        return f'Pointing: obstime={self.obstime}  ra={self.ra}  dec={self.dec} pos={self.pos1,self.pos2,self.pos3}'
-
-
-
-
-
-
-
-
-# Main functional classes for MPChecker2
-# --------------------------------------------------------------
-
-class MPCHECKER:
-    '''
-        Provide all methods associated with MPChecker
+        Provide all methods associated with "Checking"
          - Get "shortlists" for night
          - Do detailed orbital advance
          - Get "refined" list of actual overlap with FoV
          - Associated refined list with detections/tracklets
+         
+         *** AS OF 2020_07_31, MUCH OF THE CODE IN HERE SHOULD BE    ***
+         *** VIEWED AS PSEUDO-CODE, JUST SKETCHING OUT APPROXIMATELY ***
+         *** HOW IT COULD/SHOULD WORK. NO TESTING HAS BEEN DONE      ***
+
         
     '''
     
     
-    def __init__(self, ):
-        '''
-            ...
-        '''
-        self.suspects = None
+    def __init__(self,):
+        ''' Empty initialization '''
+        self.MSCs = None
     
+    def posn_check(self, primary_unpacked_provisional_designation, detections , param_dict = None ):
+    ''' pCheck: Do ephemeris look-up & calculate residuals w.r.t. detections
+        
+        inputs:
+        -------
+        primary_unpacked_provisional_designation : string 
+         - 
+        detections : A single *Detections* object
+         - 
+         
+        returns:
+        --------
+        None
+        
+     
+    '''
 
-    def get_objects_which_overlap_FoV( pointing_variable ):
+        # Check parameters are as required
+        assert isinstance(primary_unpacked_provisional_designation, str)
+        assert isinstance(detections, data_classes.Detections )
+        assert param_dict is None or isinstance(param_dict, dict)
+
+        # Call Ephem (ephemeris-querying object)
+        Eph = Ephem(primary_unpacked_provisional_designation , detections.obstime , observatoryXYZ = detections.pos )
+
+        # Get the predicted sky-positions
+        # - As written PCheck only for single desig, so can just expand out the returned dict which will be of length-1
+        # - These 'predictions' will be a 'Detections' object (with some cols as zeroes)
+        predictions = Eph.generate_sky_predictions()[primary_unpacked_provisional_designation]
+
+        # Initialize & return Residuals object
+        return Residuals(predictions, detections, param_dict = param_dict)
+
+    
+    def mpchecker(self,  pointings_object ):
         '''
             We want to calculate which objects are in the FoV 
             (or have some chance of being in the FoV) of a single pointing. 
             
-            We load precalculated quantities to help us understand which objects have 
-            any chance at all of being close to the FoV
-            We
+            We load precalculated quantities to help us understand which subset of
+            known objects have any chance at all of being close to the FoV
             
-            Parameters
+            We advance the subset (using chebys) to find whether or not they
+            actually are in the FoV
+            
+            Inputs
             ----------
+            pointing : a Pointing object
             
             Returns
             -------
-            suspects    :   SUSPECTS-object
-            
-            Examples
-            --------
-            >>> ...
-        
+            MSCs: a list of MSC objects
+                    
         '''
 
-        # Make sure pointing_variable is a list-of-pointings
-        list_of_pointings = self._rectify_pointings(pointing_variable)
+        # Check is *Vectorial* object
+        # N.B. Pointings & Detections will pass as they inherit from Vectorial
+        assert isinstance(pointings_object, data_classes.Vectorial)
 
         # Get precalculated nightly datasets
-        # - result_dict: key=HP ;  value=list-of-coeff-dictionaries
-        for p in list_of_pointings:
-            
-            # For a given JD, finds the object-integers for each HP in a list of HPs
-            # - These are the objects in close HP. Subsequently to be refined.
-            MSCs = precalc.PreCalc().get_nightly_precalcs( p.JDutc , p.HPlist )
+        #  - For a given JD, finds the objects that are in HPlist at the night-center
+        MSCs = precalc.PreCalc().get_nightly_precalcs( pointings_object.obstime , pointings_object.HPlist )
         
-            # Advance all objects of interest to specific time of pointing
-            # Calculate topocentric unit-vector using coefficient-dict
-            # Calculate sepn between objects & center of pointing
-            #  - Should make more specific : have some part of the their sky-plane-uncert in the FoV
-            # Check whether this is within the FoV / within the search-radius
-            MSCs = np.array( [M for M in MSCs if np.arccos(np.clip( np.dot( M.generate_UnitVector( p.JDutc , np.array([p.pos1, p.pos2, p.pos3] ) ), p.UnitVec() ) , -1, 1)) < p.radius ] )
+        # Advance all objects of interest to specific time of pointing
+        # Calculate topocentric unit-vector using coefficient-dict
+        # Calculate sepn between objects & center of pointing
+        #
+        #  - *** Should make more specific : have some part of the their sky-plane-uncert in the FoV ***
+        #
+        # Check whether this is within the FoV / within the search-radius
+        #    *** Seems like this calc could/should be a method within MSC ***
+        MSCs = [M for M in MSCs if np.arccos(np.clip( np.dot(
+                                                            M.generate_UnitVector( pointing.obstime , pointing.pos ),
+                                                            pointing.unit_vec() ) , -1, 1))
+                                    < pointing.radius ]
+        return MSCs
 
 
-        return SUSPECTS
-
-
-
-    def _rectify_pointings(self,  pointing_variable ):
+    def checkid(self, detections ):
         '''
-            Private method called to rectify the input to "get_objects_which_overlap_FoV()"
+            Find objects close to presented detections
             
-            inputs:
+            Inputs
+            ----------
+            detections : a Detections object
+            
+            Returns
             -------
-            pointing_variable: pointing or list-of-pointings
+
+        '''
+        # Check is *Detections* object
+        assert isinstance(detections, data_classes.Detections)
+
+        # Turn each detection into a "pointing" so that we can use mpchecker
+        #
+        # *** THIS SEEMS SLIGHTLY UNNECESSARY ***
+        # *** MOST/ALL OF THE NECESSARY VARIABLES ARE SHARED ACROSS Pointing & Detections
+        # *** SHOULD JUST COMBINED / INHERIT FROM COMMON PARENT
+        # *** THEN ALLOW MPCHECKER TO WORK ON EITHER
+        #
+        # NB This is setting pointing_radius == np.max(detection.rmsra, detection.rmsdec)
+        #    => mpchecker will be searching for objects within this radius
+        pseudo_pointings_list = detections.to_pointings()
+        
+        # Call mpchecker on each pointing
+        # *** We may want to make mpchecker work on lists/arrays of Pointing objects ***
+        for p in pseudo_pointings_list:
+            MSCs = self.mpchecker( p )
             
-            returns:
-            --------
-            list_of_pointings : list-of-pointings
-            -
-        
-        '''
-        # If singular quantity, make into lists
-        if isinstance(pointing_variable, POINTING):
-            list_of_pointings = [POINTING]
-            
-        # If non-singular, check plausibly formatted
-        # (N.B. Not doing detailed content checks at this point)
-        elif    isinstance(pointing_variable, (list, np.ndarray)) and \
-            np.all([ isinstance(_, POINTING) for _ in pointing_variable]):
-            list_of_pointings = pointing_variable
-        else:
-            sys.exit('Cannot process input of type %r in MPCHECKER ' % ( type(pointing_variable)) )
-                        
-        # return everything in list form
-        return list_of_pointings
+        # What's next ?!?!?!?
+        # (1) So far we have no concept of a tracklet, just individual detections:
+        #     Probably need to have them linked as tracklets...
+        # (2) Then need to introduce criteria / parameters to say whether *all*
+        #     observations in the tracklet are close-enough
 
 
-
-class IDENTIFY:   ## <<-- What is the difference between identification and attribution ?
-    '''
-    To match SUSPECTS from POINTINGs to
-    a set of detections and/or tracklets
-    '''
-    
-    # Add some content ...
-    def __init__(self , suspects, data):
-        '''
-            
-        Parameters
-        ----------
-        suspects    :   single SUSPECTS-object or list of SUSPECTS-objects
-            These are the results of running MPChecker
-        data        :   single EXPOSURE-object or list of TRACKLET-objects
-            These are the data we wish to identify (attribute?) 
-            with previously known object-orbits
-            
-        Returns
-        -------
-        ids         :   IDENTIFICATIONS-object
-        
-        
-        Examples
-        --------
-        >>> ...
-        
-        '''
-        # Single detections ...
-        if isinstance(suspects, SUSPECTS) and isinstance(data, exposure):
-            ids = identify_SUSPECTS_from_multiple_pointings_with_tracklets_from_multiple_exposures(suspects, data)
-        # Multiple detections in tracklets ...
-        elif isinstance(suspects, list) and isinstance(data, trackletList):
-            ids = identify_SUSPECTS_from_multiple_pointings_with_tracklets_from_multiple_exposures(suspects, data)
-        # Erroroneous input ...
-        else:
-            sys.exit("Incompatible data types input to IDENTIFY class")
-
-        # Does any additional reformatting or proocessing need to be done ?
-        
-        return ids
-
-
-    def identify_SUSPECTS_from_a_single_pointing_with_detections_from_a_single_exposure(suspects, exposure):
-        '''
-        
-        Parameters
-        ----------
-        suspects    :   SUSPECTS-object
-        exposure    :   EXPOSURE-object
-        
-        Returns
-        -------
-        
-        Examples
-        --------
-        >>> ...
-        
-        '''
-            
-        return IDENTIFICATIONS()
-
-    def identify_SUSPECTS_from_multiple_pointings_with_tracklets_from_multiple_exposures(suspectsList, trackletList):
-        '''
-        Identify SUSPECTS from multiple POINTINGS with a list of TRACKLETS
-        Dates of the pointings need to correspond to the dates of the tracklet detections
-
-        Parameters
-        ----------
-        suspectsList    :   list of SUSPECTS-objects
-        trackletList    :   list of TRACKLET-objects
-
-        Returns
-        -------
-
-        Examples
-        --------
-        >>> ...
-
-        '''
-
-        return IDENTIFICATIONS()
-
-
-
-
-
+    def checkidX(self, detections ):
+        ''' A version of checkid that allows varying mean anomaly / time '''
+        pass
