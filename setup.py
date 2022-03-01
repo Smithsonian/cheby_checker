54fecc64dfc66b563738273548e1e3d4b3650558
fiftymillionfeltcapfanscantbewrong <99844853+fiftymillionfeltcapfanscantbewrong@users.noreply.github.com>
2022-03-01 16:28:17 -0500

    Use requirements.txt instead of setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ import setuptools
 setuptools.setup(
     name="cheby",
     version="0.0.1",
-    # TODO: Deprecate w/ reqs
-    # install_requires=['numpy', 'pytest', 'astropy', 'astropy_healpix', 'astroquery', 'scipy', 'novas', 'novas_de405', 'jplephem'],
     packages=setuptools.find_packages()
 )
