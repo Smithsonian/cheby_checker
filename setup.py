8bfe4bc375fffe9712ff7266dd98b5618d177e3a
fiftymillionfeltcapfanscantbewrong <99844853+fiftymillionfeltcapfanscantbewrong@users.noreply.github.com>
2022-08-16 13:37:53 -0400

    I think this includes the dev_data appropriately

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setuptools.setup(
     name="cheby",
     version="0.0.1",
     packages=setuptools.find_packages(),
-    package_data={'cheby_checker': ['dev_data']},
+    package_data={'cheby_checker': ['../../dev_data/*']},
     include_package_data=True
 )
